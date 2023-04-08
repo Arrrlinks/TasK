626643132d36811a03424752682f05e5cf6e697b
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-04-08 19:18:44 +0200

    feat : logo

--- views/template.php
+++ views/template.php
@@ -11,6 +11,7 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css" rel="stylesheet">
+    <link rel="icon" href="imgs/TasK.png" type="image/png">
 </head>
 <body>
 <?= $content ?>
