5640d75c86045e8165a4689a58acfcbef24d94d8
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-04-09 22:31:54 +0200

    fix: don't activate blur when we click on the remove button next to the user input

--- scripts/addUser.php
+++ scripts/addUser.php
@@ -1,33 +1,38 @@
 <?php
 session_start();
-if(isset($_POST['user']) && isset($_POST['page'])) {
+if (isset($_POST['user']) && isset($_POST['page'])) {
     $user = $_POST['user'];
     $page = $_POST['page'];
     require_once('../src/init.php');
     $db = dbConnect();
     $req = $db->prepare('SELECT id FROM users WHERE email = ?');
     $req->execute(array($user));
+    $req2 = $db->prepare('SELECT users.email FROM pages INNER JOIN users ON pages.idOwner = users.id WHERE pages.id = ?');
+    $req2->execute(array($page));
     $idUser = $req->fetch();
-    if($idUser) {
-        $req = $db->prepare('SELECT idUser FROM sharedPages WHERE idUser = ? AND idPage = ?');
-        $req->execute(array($idUser['id'], $page));
-        $result = $req->fetch();
-        if($result) {
-            echo 'alreadyAdded';
-        }
-        else {
-            $req = $db->prepare('INSERT INTO sharedPages(idUser, idPage) VALUES(?, ?)');
-            $req->execute(array($idUser['id'], $page));
-            $req = $db->prepare('INSERT INTO notifications(idUser,idSender, idPage) VALUES(?,?,?)');
-            $req->execute(array($idUser['id'], $_SESSION['id'], $page));
-            $req = $db->prepare('SELECT id,firstName,lastName,email FROM users WHERE email = ?');
-            $req->execute(array($user));
-            $result = $req->fetch();
-            echo json_encode($result);
-        }
-
+    $idOwner = $req2->fetch();
+    if ($user == $idOwner['email']) {
+        echo 'owner';
     }
     else {
-        echo 'invalid';
+        if ($idUser) {
+            $req = $db->prepare('SELECT idUser FROM sharedPages WHERE idUser = ? AND idPage = ?');
+            $req->execute(array($idUser['id'], $page));
+            $result = $req->fetch();
+            if ($result) {
+                echo 'alreadyAdded';
+            } else {
+                $req = $db->prepare('INSERT INTO sharedPages(idUser, idPage) VALUES(?, ?)');
+                $req->execute(array($idUser['id'], $page));
+                $req = $db->prepare('INSERT INTO notifications(idUser,idSender, idPage) VALUES(?,?,?)');
+                $req->execute(array($idUser['id'], $_SESSION['id'], $page));
+                $req = $db->prepare('SELECT id,firstName,lastName,email FROM users WHERE email = ?');
+                $req->execute(array($user));
+                $result = $req->fetch();
+                echo json_encode($result);
+            }
+        } else {
+            echo 'invalid';
+        }
     }
 }
--- scripts/users.js
+++ scripts/users.js
@@ -6,16 +6,21 @@ function addUser() {
     userDiv.setAttribute('class', 'user');
     userDiv.innerHTML = `
             <input type="text" name="user" id="user" placeholder="Enter the email of the user you want to add" required>
-            <button onclick="removeInput()"><ion-icon name="close-outline"></ion-icon></button>
+            <button id="removeInput" ><ion-icon name="close-outline"></ion-icon></button>
     `;
     const user = document.getElementById('users');
     user.append(userDiv);
+    const removeInput = document.getElementById('removeInput');
+    removeInput.addEventListener('click', function(event) {
+        const user = event.target.parentNode;
+        user.parentNode.removeChild(user);
+        stop();
+    });
     input[input.length-1].focus();
     for (let i = 0; i < input.length; i++) {
         let value = input[i].value;
         input[i].addEventListener('blur', function(event) {
-            // detect if the button is clicked
-            if (value !== event.target.value && event.target.parentNode.getElementsByTagName('button')[0].getAttribute('onclick') === '') {
+            if (value !== event.target.value) {
                 const xhr = new XMLHttpRequest();
                 xhr.open('POST', '../scripts/addUser.php', true);
                 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -25,6 +30,9 @@ function addUser() {
                     }
                     else if (this.responseText === 'alreadyAdded') {
                         alertUserAlreadyAdded();
+                    }
+                    else if (this.responseText === 'owner') {
+                        alertUserIsOwner();
                     }
                     else {
                         const user = JSON.parse(this.responseText);
@@ -50,12 +58,7 @@ function addUser() {
     }
 }
 
-function removeInput() {
-    const userDiv = document.getElementById('user').parentNode;
-    userDiv.parentNode.removeChild(userDiv);
-}
-
-window.addEventListener('beforeunload', function (e) {
+window.addEventListener('beforeunload', function (event) {
     event.target.blur();
 });
 
@@ -63,7 +66,6 @@ function removeUser(id,page) {
     // remove the parent element of the button
     const idUser = document.getElementById(id);
     const user = idUser.parentNode;
-    console.log(user);
     user.parentNode.removeChild(user);
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '../scripts/removeUser.php', true);
@@ -86,6 +88,15 @@ function alertUserDoesntExist() {
 function alertUserAlreadyAdded() {
     Swal.fire({
         title: 'User already added',
+        text: 'Please enter a valid email',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+    });
+}
+
+function alertUserIsOwner() {
+    Swal.fire({
+        title: 'You can\'t add yourself',
         text: 'Please enter a valid email',
         icon: 'error',
         confirmButtonText: 'Ok'
